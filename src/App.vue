f9d055e169db7c8decd8569fe04811ae4e9cec15
lixiaoni <1165388551@qq.com>
2018-12-06 11:54:39 +0800

    12.6

--- src/App.vue
+++ src/App.vue
@@ -19,7 +19,7 @@ export default{
   .block .el-form-item__content input, .block .el-form-item__content select{
     width: 200px;
   }
- .block .el-form-item--small{
+ .block .el-form-item--small,.block .el-form--inline .el-form-item{
     width: 100%
   }
 </style>
