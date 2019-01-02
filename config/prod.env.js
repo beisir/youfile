ab90fe956709c96b938264cc76c9f35752192e8c
lixiaoni <1165388551@qq.com>
2019-01-02 09:44:30 +0800

    1.2

--- config/prod.env.js
+++ config/prod.env.js
@@ -2,5 +2,5 @@ module.exports = {
   NODE_ENV: '"production"',
   ENV_CONFIG: '"prod"',
   BASE_API: '"https://bossapi.youlife.me"',
-  IMAGE_UPLOAD_API: '"http://store.youlife.me/base"'
+  IMAGE_UPLOAD_API: '"https://store.youlife.me/base"'
 }
