512b3cea254f95708fd096afb4d83588fac5b141
shengya.huang <huangshengya6740@163.com>
2019-01-11 18:52:54 +0800

    优生活beta环境添加

--- config/beta.env.js
+++ config/beta.env.js
@@ -1,5 +1,5 @@
 module.exports = {
-  NODE_ENV: '"production"',
+  NODE_ENV: '"beta"',
   ENV_CONFIG: '"beta"',
   BASE_API: '"https://bossapi.youlife.me"',
   IMAGE_UPLOAD_API: '"https://store.youlife.me"'
