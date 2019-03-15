f2a67c5ccd410b5732964b8dc51f1e4be5e1275d
shengya.huang <huangshengya6740@163.com>
2019-03-15 17:12:29 +0800

    修改地址

--- config/sit.env.js
+++ config/sit.env.js
@@ -1,6 +1,6 @@
 module.exports = {
-  NODE_ENV: '"beta"',
-  ENV_CONFIG: '"beta"',
-  BASE_API: '"https://beta-bossapi.youlife.net.cn"',
-  IMAGE_UPLOAD_API: '"https://beta-store.youlife.net.cn"'
+  NODE_ENV: '"dev"',
+  ENV_CONFIG: '"dev"',
+  BASE_API: '"https://dev-bossapi.youlife.net.cn"',
+  IMAGE_UPLOAD_API: '"https://dev-store.youlife.net.cn"'
 }
